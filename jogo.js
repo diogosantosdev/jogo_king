c7f8ce3deca7c4f456bdcc24c8b5ed1530f2f774
diogosantosdev <38300678+diogosantosdev@users.noreply.github.com>
2021-02-08 03:27:00 +0000

    Add files via upload

--- jogo.js
+++ jogo.js
@@ -2,9 +2,22 @@
 let width = 10;
 let height = 4;
 
+//board com json
+
+
+/*let board = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0]
+];*/
+
+
+
 
 //criação dos jogadores
-players = [{name: "jogador 1",position: 0,color: "blue"},{name: "jogador 2",position: 0,color: "green"}];
+players = [{name: "Jogador 1",position: 0,color: "blue"},{name: "Jogador 2",position: 0,color: "green"}];
 
 let currentPlayerTurn = 0;
 
@@ -25,7 +38,10 @@ let currentPlayer = players[currentPlayerTurn];
 //roll do dado random
 
 roll = Math.floor(Math.random() * 5 + 1);
-console.log(currentPlayer.name +", saiu um ", roll); //imprime o valor que saiu ao jogador
+let roll1=document.getElementById("roll1")
+ roll1.innerHTML=currentPlayer.name +", saiu um " + roll;
+ 
+
 
   //aumento da posição atual do jogador
 
@@ -53,33 +69,84 @@ console.log(currentPlayer.name +", saiu um ", roll); //imprime o valor que saiu 
 
 createBoard(); }
 
+//for (i=position; position <= boardSize ;i++) {
+
+  //window.square.innerHTML == position[i];
+
+  //}
+
+
+  /*board.numerar(square,i => {
+
+    for(i=1 ; i<=boardSize ;i++) {
+
+      square.innerHTML == [i]
+    }
+
+  })*/
 
 for (let y = height; y >= 0; y--) {
   let row = [];
 
   board.push(row);
+
   for (let x = 0; x < width; x++) {
-    row.push({x,y,occupied: null,position  });
+    row.push({x,y,position, }); 
     position++;
+
   }
+
 }
 
 
-boardSize = 50;
+
+let boardSize = 50;
+
+
+
+
 createBoard = () => {
 
-  let boardOnScreen = ``;
+  let boardOnScreen = ``; //posição da board 
   board.forEach(row => {
     row.forEach(square => {
-    boardOnScreen = boardOnScreen + `<div class=square style="top:${square.y * boardSize}px; left:${square.x * boardSize}px; background-color:${square.color}"></div>`;
 
-    });
+      for (let i=0 ; i <= square.position +1  ; i++) {
+    
+    boardOnScreen = boardOnScreen + `<div class=square style="top:${square.y * boardSize}px; left:${square.x * boardSize}px; background-color:${square.color}">${[i]}</div>`;
+ 
+  
+  
+      }
+
+    }); // iteração do numero para cada square ( duvida ! ) . square.position
+    
+    
 
   });
 
-  players.forEach(player => {
+  /*function numbers() {
 
-    let square = null;
+      let boxes = document.square.innerHTML;
+
+      boxes.forEach((box,i)=> {
+
+        for(i=1;i<boardSize;i++){
+
+          box.innerHTML=[i];
+        }
+
+        return boxes
+      }) 
+
+
+
+  } */
+
+  
+players.forEach(player => {
+
+     //posição do player conforme a board
     board.forEach(row => {
       row.forEach(square => {
         if (square.position == player.position) {
